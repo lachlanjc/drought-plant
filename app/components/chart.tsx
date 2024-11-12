ec4616f6dc698e4b44ddc5e454ae73ae7b7ba158
Lachlan Campbell <lachlanjc@hey.com>
2024-11-12 02:36:43 -0500

    Add new files

---
+++ app/components/chart.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { Bar, BarChart, CartesianGrid, XAxis } from "recharts";
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
+import { HistoricalRainfall } from "../api/route";
+
+const chartConfig = {
+  precip: {
+    label: "Precip.",
+    color: "#2563eb",
+  },
+  // mobile: {
+  //   label: "Mobile",
+  //   color: "#60a5fa",
+  // },
+} satisfies ChartConfig;
+
+export function Chart({ data }: { data: HistoricalRainfall }) {
+  return (
+    <ChartContainer config={chartConfig} className="h-[200px] w-full">
+      <BarChart accessibilityLayer data={data}>
+        <CartesianGrid horizontal={false} />
+        <Bar dataKey="precip" fill="var(--color-precip)" radius={4} />
+        {/* <Bar dataKey="mobile" fill="var(--color-mobile)" radius={4} /> */}
+        <ChartTooltip content={<ChartTooltipContent />} />
+        <XAxis
+          dataKey="dt"
+          tickLine={false}
+          // tickMargin={10}
+          axisLine={false}
+          tickFormatter={(value) =>
+            new Date(value).toLocaleDateString("en-US", {
+              month: "short",
+              day: "numeric",
+            })
+          }
+        />
+      </BarChart>
+    </ChartContainer>
+  );
+}
